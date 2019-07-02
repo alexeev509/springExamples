cf75014f67d468ea5e4efb33efc26f072c8ce13c
Tanya Tarinskaya <tanya.tarinskaya@gmail.com>
2019-07-02 19:16:47 +0300

    Compare two realizations of Spring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        //With annotations
+        //With annotations:
         ApplicationContext ctx = new AnnotationConfigApplicationContext("beans");
         Car car1 = ctx.getBean(Car.class);
         System.out.println(car1.toString());
--- src/main/java/beans/Car.java
+++ src/main/java/beans/Car.java
@@ -3,6 +3,8 @@ package beans;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
 public class Car {
     @Value("Volkswagen")
@@ -19,6 +21,21 @@ public class Car {
                 "marka='" + marka + '\'' +
                 '}';
     }
+
+    public Car() {
+        System.out.println("I am here");
+    }
+
+    @PostConstruct
+    public void startMethodForAnnotation() {
+        System.out.println("It will be after object will be created for annotations");
+    }
+
+    public void startMethodForXml() {
+        System.out.println("It will be after object will be created for xml");
+    }
+
+
 
     //will be exception because we have 2 beans Car
 //    @Bean
