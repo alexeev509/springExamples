68f70f12de26edaa3b632daa087d25a407df5bde
Tanya Tarinskaya <tanya.tarinskaya@gmail.com>
2019-07-02 19:07:38 +0300

    Compare two realizations of Spring

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,18 @@
 import beans.Car;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.StaticApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         //With annotations
         ApplicationContext ctx = new AnnotationConfigApplicationContext("beans");
-        Car course = (Car) ctx.getBean(Car.class);
+        Car car1 = ctx.getBean(Car.class);
+        System.out.println(car1.toString());
 
         //with xml:
+        ApplicationContext context = new ClassPathXmlApplicationContext("configFile.xml");
+        Car car2 = context.getBean(Car.class);
+        System.out.println(car2.toString());
     }
 }
--- src/main/java/beans/Car.java
+++ src/main/java/beans/Car.java
@@ -1,10 +1,26 @@
 package beans;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Car {
+    @Value("Volkswagen")
+    private String marka;
+
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "marka='" + marka + '\'' +
+                '}';
+    }
+
+    //will be exception because we have 2 beans Car
 //    @Bean
 //    public Car car() {
 //        return new Car();
